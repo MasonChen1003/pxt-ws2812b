734926955f4f1a1f1488f27496164cb7ad54eb14
Shiun Robot DIY <masonchen1003@gmail.com>
2024-05-31 06:51:25 +0800

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,10 +1,10 @@
 namespace ws2812b {
     //% shim=sendBufferAsm
-    export function sendBuffer(buf: Buffer, pin: DigitalInOutPin) {
+    export function sendBuffer(buf: Buffer, pin: digitalWritePin) {
     }
 
     //% shim=setBufferMode
-    export function setBufferMode(pin: DigitalInOutPin, mode: number) {
+    export function setBufferMode(pin: digitalWritePin, mode: number) {
 
     }
 
