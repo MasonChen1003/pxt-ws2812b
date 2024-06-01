571b216a4cb47c3564c8f248e040e7352335dadf
Shiun Robot DIY <masonchen1003@gmail.com>
2024-06-01 12:06:09 +0800

    Update sendBuffer.asm

--- sendBuffer.asm
+++ sendBuffer.asm
@@ -13,7 +13,7 @@ sendBufferAsm:
     ; 设置 pin 为 digital
     mov r0, r6           ; 引脚对象
     movs r1, #1          ; 设置为高电平
-    bl DigitalInOutPin::digitalWrite  ; 写入数字值
+    bl setPinState       ; 调用设置引脚状态的函数
 
     ; 获取 pin 地址
     mov r0, r6
